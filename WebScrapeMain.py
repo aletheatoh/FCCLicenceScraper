3d2894b8c3dfa97a41238d8183fd801cd965a5ae
Alethea Toh <alethea.toh@duke.edu>
2017-11-16 17:47:36 -0500

    addressed call signs with TP status

--- WebScrapeMain.py
+++ WebScrapeMain.py
@@ -17,12 +17,17 @@ ws1 = wb.add_sheet('Sheet 1',cell_overwrite_ok=True) # sheet 1 contains all the 
 ws2 = wb.add_sheet('Sheet 2',cell_overwrite_ok=True) # sheet 2 contains two list of subsidiary companies: 1) returned results 2) did not return results
     
 rowTracker = 1 # to track row in excel spreadsheet
+subTracker = "" # to track subsidiary we are searching
+
 subsWithResults = set([]) # set of subsidiaries that are returning results
 subsWithoutResults = set([]) # set of subsidiaries that are NOT returning results
+subsTermPending = set([]) # set of subsidiaries that have call signs or components of call signs that are in termination pending status
 
 def extractData(sub):
     global rowTracker
-
+    global subTracker
+    subTracker = sub
+    
     # number of rows = number of call sign/lease IDs: -2 from row_count two remove header/footer rows
     row_count = len(driver.find_elements_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table/tbody/tr[5]/td/table[1]/tbody[1]/tr")) - 2
     
@@ -38,16 +43,17 @@ def extractData(sub):
                 ws1.row(rowTracker).write(numC, sub)
                 subsWithResults.add(sub) # keep adding subsidiaries that return results to set
             elif numC > 0:
-                ws1.row(rowTracker).write(numC, value.text)
+                ws1.row(rowTracker).write(numC, value.text.strip())
+            wb.save('/Users/alethea/Downloads/Spreadsheet_test.xls')
             numC += 1
             
         callSignDataButton = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table/tbody/tr[5]/td/table[1]/tbody/tr[" + str(numR) + "]/td[2]/a")
         callSignDataButton.click()  
-    
-        callSignData(rowTracker,numC)
          
+        callSignData(rowTracker,numC)
+
         rowTracker += 1
-    
+        
     try:
         nextPage = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table/tbody/tr[5]/td/table[2]/tbody/tr/td/table/tbody/tr/td[3]/a")
         nextPage.click()
@@ -56,51 +62,107 @@ def extractData(sub):
         pass
     
 def callSignData(rowNum,colNum):
-    # scrape market section
-    market = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[8]/td[2]")
-    ws1.row(rowNum).write(colNum, market.text)
-    submarket = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[9]/td[2]")
-    ws1.row(rowNum).write(colNum+1, submarket.text)
-    channelBlock = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[8]/td[4]")
-    ws1.row(rowNum).write(colNum+2, channelBlock.text)
-    associatedFreq = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[9]/td[4]")
-    ws1.row(rowNum).write(colNum+3, associatedFreq.text)
+    # need to determine if call sign or component(s) of call sign is in termination pending status
+    tp = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]")
     
-    # scrape dates section
-    grant = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[11]/td[2]")
-    ws1.row(rowNum).write(colNum+4, grant.text)
-    effective = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[12]/td[2]")
-    ws1.row(rowNum).write(colNum+5, effective.text)
-    expiration = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[11]/td[4]")
-    ws1.row(rowNum).write(colNum+6, expiration.text)
-    cancellation = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[12]/td[4]")
-    ws1.row(rowNum).write(colNum+7, cancellation.text)
+    if tp.text.strip() == "This call sign or a component(s) of this call sign is in termination pending status for failure to meet the buildout requirement.":
+        subsTermPending.add(subTracker)
         
-    # scrape buildout deadlines
-    firstBD = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[15]/td[2]")
-    ws1.row(rowNum).write(colNum+8, firstBD.text)
-    secondBD = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[15]/td[4]")
-    ws1.row(rowNum).write(colNum+9, secondBD.text)
+        market = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[3]/td/table/tbody/tr[8]/td[2]")
+        ws1.row(rowNum).write(colNum, market.text.strip())
+        submarket = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[3]/td/table/tbody/tr[9]/td[2]")
+        ws1.row(rowNum).write(colNum+1, submarket.text.strip())
+        channelBlock = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[3]/td/table/tbody/tr[8]/td[4]")
+        ws1.row(rowNum).write(colNum+2, channelBlock.text.strip())
+        associatedFreq = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[3]/td/table/tbody/tr[9]/td[4]")
+        ws1.row(rowNum).write(colNum+3, associatedFreq.text.strip())
+        wb.save('/Users/alethea/Downloads/Spreadsheet_test.xls')
         
-    # scrape licensee ID
-    licenseeID = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[4]/td/table/tbody/tr[2]/td[2]") 
-    ws1.row(rowNum).write(colNum+10, licenseeID.text)
+        # scrape dates section
+        grant = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[3]/td/table/tbody/tr[11]/td[2]")
+        ws1.row(rowNum).write(colNum+4, grant.text.strip())
+        effective = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[3]/td/table/tbody/tr[12]/td[2]")
+        ws1.row(rowNum).write(colNum+5, effective.text.strip())
+        expiration = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[3]/td/table/tbody/tr[11]/td[4]")
+        ws1.row(rowNum).write(colNum+6, expiration.text.strip())
+        cancellation = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[3]/td/table/tbody/tr[12]/td[4]")
+        ws1.row(rowNum).write(colNum+7, cancellation.text.strip())
+            
+        # scrape buildout deadlines
+        firstBD = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[3]/td/table/tbody/tr[15]/td[2]")
+        ws1.row(rowNum).write(colNum+8, firstBD.text.strip())
+        secondBD = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[3]/td/table/tbody/tr[15]/td[4]")
+        ws1.row(rowNum).write(colNum+9, secondBD.text.strip())
+        wb.save('/Users/alethea/Downloads/Spreadsheet_test.xls')
         
-    # click 'market' tab
-    marketTab = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[1]/tbody/tr[2]/td/a[2]")
-    marketTab.click()
-    # NOTE: Market tab takes quite long to load
+        # scrape licensee ID
+        licenseeID = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[5]/td/table/tbody/tr[2]/td[2]") 
+        ws1.row(rowNum).write(colNum+10, licenseeID.text.strip())
+        wb.save('/Users/alethea/Downloads/Spreadsheet_test.xls')
+
+        # click 'market' tab
+        marketTab = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[1]/tbody/tr[2]/td/a[2]")
+        marketTab.click()
+        # NOTE: Market tab takes quite long to load
+        
+        # scrape auction data - just the text and not the link
+        auction = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[5]/td[2]")
+        ws1.row(rowNum).write(colNum+11, auction.text.strip())
+        wb.save('/Users/alethea/Downloads/Spreadsheet_test.xls')
+
+        # return to results    
+        returnToResults = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/table/tbody/tr[2]/td/span[3]/a[2]")
+        returnToResults.click()
     
-    # scrape auction data - just the text and not the link
-    auction = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[4]/td[2]")
-    ws1.row(rowNum).write(colNum+11, auction.text)
+    else: # active status
+        # scrape market section
+        market = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[8]/td[2]")
+        ws1.row(rowNum).write(colNum, market.text.strip())
+        submarket = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[9]/td[2]")
+        ws1.row(rowNum).write(colNum+1, submarket.text.strip())
+        channelBlock = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[8]/td[4]")
+        ws1.row(rowNum).write(colNum+2, channelBlock.text.strip())
+        associatedFreq = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[9]/td[4]")
+        ws1.row(rowNum).write(colNum+3, associatedFreq.text.strip())
+        wb.save('/Users/alethea/Downloads/Spreadsheet_test.xls')
+        
+        # scrape dates section
+        grant = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[11]/td[2]")
+        ws1.row(rowNum).write(colNum+4, grant.text.strip())
+        effective = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[12]/td[2]")
+        ws1.row(rowNum).write(colNum+5, effective.text.strip())
+        expiration = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[11]/td[4]")
+        ws1.row(rowNum).write(colNum+6, expiration.text.strip())
+        cancellation = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[12]/td[4]")
+        ws1.row(rowNum).write(colNum+7, cancellation.text.strip())
+            
+        # scrape buildout deadlines
+        firstBD = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[15]/td[2]")
+        ws1.row(rowNum).write(colNum+8, firstBD.text.strip())
+        secondBD = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[15]/td[4]")
+        ws1.row(rowNum).write(colNum+9, secondBD.text.strip())
+        wb.save('/Users/alethea/Downloads/Spreadsheet_test.xls')
+        
+        # scrape licensee ID
+        licenseeID = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[4]/td/table/tbody/tr[2]/td[2]") 
+        ws1.row(rowNum).write(colNum+10, licenseeID.text.strip())
+        wb.save('/Users/alethea/Downloads/Spreadsheet_test.xls')
+        
+        # click 'market' tab
+        marketTab = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[1]/tbody/tr[2]/td/a[2]")
+        marketTab.click()
+        # NOTE: Market tab takes quite long to load
+        
+        # scrape auction data - just the text and not the link
+        auction = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/div/table[2]/tbody/tr[2]/td/table/tbody/tr[4]/td[2]")
+        ws1.row(rowNum).write(colNum+11, auction.text.strip())
+        wb.save('/Users/alethea/Downloads/Spreadsheet_test.xls')
+        
+        # return to results    
+        returnToResults = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/table/tbody/tr[2]/td/span[3]/a[2]")
+        returnToResults.click()
     
-    # return to results    
-    returnToResults = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/table/tbody/tr[2]/td/span[3]/a[2]")
-    returnToResults.click()
-    
-def subSearch(sub):
-    
+def subSearch(sub): 
     # click the required radio service code options
     rsc = Select(driver.find_element_by_xpath("//select[@name='radioservicecode']"))
     radioServiceCodes = ["AH", "AT", "AW", "CN", "CW", "CY", "LD", "SG", "SL", "SP", "SY", "TZ", "WP", "WU", "WX", "WY", "WZ"] 
@@ -115,10 +177,10 @@ def subSearch(sub):
     search = driver.find_element_by_xpath("//input[@src='external/buttons/newsearch-blue.gif']")
     search.click()
     
-#     # start web scraping the subsidiary data
+    # start web scraping the subsidiary data
     extractData(sub)
-#      
-#     # go back to new search
+    
+    # go back to new search
     newSearch = driver.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/table/tbody/tr[2]/td/span[1]/a[2]")
     newSearch.click()
 
@@ -149,12 +211,14 @@ if __name__ == '__main__':
     # actual implementation: createDict(data) returns a set of all the subsidiary companies (so this removes duplicates)
     for subsidiary in createDict(data):
         subSearch(subsidiary)
+        wb.save('/Users/alethea/Downloads/Spreadsheet_test.xls')
  
     # set of subsidiaries that are NOT returning results
     subsWithoutResults = createDict(data) - subsWithResults
     
     ws2.row(0).write(0, "Returned Results",style)
     ws2.row(0).write(1, "No Results",style)
+    ws2.row(0).write(2, "Call Signs with Termination Pending Status",style)
     
     idx1 = 1
     for sub in subsWithResults:
@@ -165,6 +229,11 @@ if __name__ == '__main__':
     for sub in subsWithoutResults:
         ws2.row(idx2).write(1, sub)
         idx2+=1
+    
+    idx3 = 1
+    for sub in subsTermPending:
+        ws2.row(idx3).write(2, sub)
+        idx3+=1
     
     wb.save('/Users/alethea/Downloads/Spreadsheet_test.xls')
     
